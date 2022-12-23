055a5f044659f909b6c26506122420b054ec5ba3
SedaNoty <109861733+SedaNoty@users.noreply.github.com>
2022-12-23 15:18:34 +0000

    Deploying to gh-pages from @ SedaNoty/goit-js-hw-09@7c930fa633bf1deb6739a68c5a8d9b2f24ca4027 🚀

---
+++ 01-color-switcher.0670e1d1.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");e.disabled=!0;var a=0;function d(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval(d,1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.0670e1d1.js.map
