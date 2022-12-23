7c930fa633bf1deb6739a68c5a8d9b2f24ca4027
SedaNoty <sedanoty5@gmail.com>
2022-12-23 17:18:04 +0200

    create-goit-js-hw-09_part-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const clickStart = document.querySelector('button[data-start]');
+const clickStop = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector('body');
+
+clickStop.disabled = true;
+let timerId = 0;
+
+clickStart.addEventListener('click', () => {
+  clickStart.disabled = true;
+  clickStop.disabled = false;
+
+  timerId = setInterval(changingColor, 1000);
+});
+
+function changingColor() {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+clickStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  clickStart.disabled = false;
+  clickStop.disabled = true;
+});
